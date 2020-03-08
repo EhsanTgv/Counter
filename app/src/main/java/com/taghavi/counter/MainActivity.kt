e554ff5a27c71968483f7ff71f0f14d163577676
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-08 23:42:29 +0330

    Add simple ClickListener Event

--- app/src/main/java/com/taghavi/counter/MainActivity.kt
+++ app/src/main/java/com/taghavi/counter/MainActivity.kt
@@ -2,11 +2,18 @@ package com.taghavi.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var numTimeClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainButton.setOnClickListener {
+            numTimeClicked += 1
+            mainTextView.append("\nThe Button got tapped $numTimeClicked time")
+        }
     }
 }
