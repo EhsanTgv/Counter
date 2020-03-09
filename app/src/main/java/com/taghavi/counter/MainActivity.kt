f4eef5e596a022dc4aad23bbcfb8f6b0e0a18787
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-09 22:44:59 +0330

    Make TextView Scrollable

--- app/src/main/java/com/taghavi/counter/MainActivity.kt
+++ app/src/main/java/com/taghavi/counter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.taghavi.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainTextView.movementMethod = ScrollingMovementMethod()
 
         mainButton.setOnClickListener {
             numTimeClicked += 1
