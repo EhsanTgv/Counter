cb87727a0439e1b5fb2535772414aa494ec66af5
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-10 22:57:09 +0330

    Saving and Restoring Instance State.mp4

--- app/src/main/java/com/taghavi/counter/MainActivity.kt
+++ app/src/main/java/com/taghavi/counter/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val TEXT_CONTENTS = "TextContent"
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +20,15 @@ class MainActivity : AppCompatActivity() {
             mainTextView.append("\n")
             mainEditText.setText("")
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        mainTextView.text = savedInstanceState.getString(TEXT_CONTENTS, "")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_CONTENTS, mainTextView.text.toString())
     }
 }
